5c92cea40a385f092e4b5353be4ad060321ea668
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2018-10-17 17:47:13 -0300

    Aula 42 - Injetando um serviço em outro serviço https://youtu.be/hMhCDO75Aus

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,9 @@ import { CursosModule } from './cursos/cursos.module';
     CriarCursoModule,
     CursosModule
   ],
+  providers: [
+    LogService
+  ]
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, EventEmitter } from "@angular/core";
+import { LogService } from "../shared/log.service";
 
 @Injectable()
 export class CursosService {
@@ -8,15 +9,17 @@ export class CursosService {
     
     private cursos: string[] = [ "Angular", "Java", "PhoneGap" ];
 
-    constructor() {
+    constructor(private logService: LogService) {
         console.log("CursosService");
     }
 
     getCursos() {
+        this.logService.log("Obtendo lista de cursos");
         return this.cursos;
     }
 
     addCurso(curso: string) {
+        this.logService.log(`Criando um novo curso ${curso}`);
         this.cursos.push(curso);
         this.emitirCursoCriado.emit(curso);
         CursosService.criouNovoCurso.emit(curso);
