60532ae6b3acd34fed898f15c6d4f3ff95e897ff
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2018-10-19 12:24:36 -0300

    Aula 50 - Rotas: Configurando rotas simples https://youtu.be/8OHoAZ6j0Rg

---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+const APP_ROUTES: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'cursos',
+        component: CursosComponent
+    }
+];
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
