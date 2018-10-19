74116d02b1cbc511d9d81f6d35053afb09323844
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2018-10-19 14:42:22 -0300

    Aula 54 - Rotas: Escutando mudanças nos parâmetros de roteamento https://youtu.be/a6PjrYAPrGs

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -8,14 +9,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CursoDetalheComponent implements OnInit {
   
-
   id: string;
+  inscricao: Subscription
 
   constructor(private route: ActivatedRoute ) {
-    this.id = route.snapshot.params['id'];
+    // this.id = route.snapshot.params['id'];
   }
 
   ngOnInit() {
+    this.inscricao = this.route.params.subscribe((params:any) => {
+      this.route = params['id'];
+    });
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
