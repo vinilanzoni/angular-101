92d3cb8aad20a6b08fb0b57028224144d5c9cb9e
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2018-10-23 18:24:41 -0300

    Aula 70 - Definindo rota padrão e wildcard (rota não encontrada) https://youtu.be/PGJeWEsejdA

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
 import { AlunosGuard } from './guards/alunos.guard';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 // import { CursosModule } from './cursos/cursos.module';
 // import { AlunosModule } from './alunos/alunos.module';
 
@@ -20,7 +21,8 @@ import { AlunosGuard } from './guards/alunos.guard';
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -5,13 +5,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 // import { CursosComponent } from './cursos/cursos.component';
 // import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 // import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 const APP_ROUTES: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
   // { path: 'cursos', component: CursosComponent },
   // { path: 'curso/:id', component: CursoDetalheComponent },
   // { path: 'not-found', component: CursoNaoEncontradoComponent }
@@ -27,7 +26,11 @@ const APP_ROUTES: Routes = [
     loadChildren: './alunos/alunos.module#AlunosModule', 
     canActivate: [AuthGuard],
     canLoad: [AuthGuard]
-  }
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
