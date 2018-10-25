9643748ac768595c700ccb3b47358c63fc0a211a
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2018-10-25 17:40:49 -0300

    Aula 95 - Formulários reativos: Acesso ao FormControl no HTML e CSS de validação dos campos https://youtu.be/srtenF1rFYI

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -3,18 +3,16 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-data-form',
-  templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.css']
+  selector: "app-data-form",
+  templateUrl: "./data-form.component.html",
+  styleUrls: ["./data-form.component.css"]
 })
 export class DataFormComponent implements OnInit {
-
   formulario: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit() {
-
     // this.formulario = new FormGroup({
     //   inputNome: new FormControl(null),
     //   inputEmail: new FormControl(null)
@@ -30,16 +28,29 @@ export class DataFormComponent implements OnInit {
     console.log(this.formulario);
     this.http
       .post("https://httpbin.org/post", JSON.stringify(this.formulario.value))
-      .subscribe(res => {
-        console.log(res);
-        this.formulario.reset();
-      }, (err: any) => {
-        console.log(err);
-      });
+      .subscribe(
+        res => {
+          console.log(res);
+          this.formulario.reset();
+        },
+        (err: any) => {
+          console.log(err);
+        }
+      );
   }
 
   cancel() {
     this.formulario.reset();
   }
 
+  inputErrored(field) {
+    return !this.formulario.get(field).valid && this.formulario.get(field).touched;
+  }
+
+  invalidEmail(field) {
+    let emailField = this.formulario.get(field)
+    if(emailField.errors) {
+      return emailField.errors['email'] && emailField.touched;
+    }
+  }
 }
