2b200f715732d700713235756d7b51648793fbcb
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2018-10-25 15:32:33 -0300

    Aula 90 - Formulários reativos: Criando um form com código Angular https://youtu.be/931fynCdTKw

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -10,9 +10,19 @@ export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    // this.formulario = new FormGroup({
+    //   inputNome: new FormControl(null),
+    //   inputEmail: new FormControl(null)
+    // });
+
+    this.formulario = this.formBuilder.group({
+      inputNome: [null],
+      inputEmail: [null]
+    });
   }
 
 }
