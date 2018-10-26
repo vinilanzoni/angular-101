a33946fcdf81182469671dfb0df6ea2b34cbc418
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2018-10-26 19:01:50 -0300

    Aula 118 - Instalando Bootstrap 4 https://youtu.be/Vgj9N_Lw-r4

--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CollapseModule } from "ngx-bootstrap";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
